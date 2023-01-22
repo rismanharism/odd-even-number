58f7557335bc2a350bc1b86775e42fe76b08d14e
rismanharism <risman.haris6@gmail.com>
2023-01-22 17:35:57 +0700

    first commit

---
+++ 2.js
@@ -0,0 +1,8 @@
+for (let i = 50; i <= 200; i += 5) {
+    if (i % 3 === 0) {
+        console.log(`${i} - faktor 3`);
+    }
+    else {
+        console.log(`${i} - tidak bisa dibagi 3`);
+    }
+}
---
+++ 3.js
@@ -0,0 +1,6 @@
+for (let i = 100; i <= 200; i += 7) {
+    if (i % 8 === 0) {
+        console.log(i);
+    }
+}
+
